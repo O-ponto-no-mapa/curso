fdc6f46e473ad558c70323081510729a3d260694
Artur Cordeiro <artur.cordeiro@gmail.com>
2023-05-27 22:00:16 -0300

    Add files via upload

---
+++ 9_iconeMarcador/mapa.js
@@ -0,0 +1,75 @@
+// Cria a variável tipo lista (array) para ser usada com os dados do arquivo JSON externo.
+let lugares = [];
+
+// Cria a variável tipo lista (array) para ser usada com os marcadores do mapa.
+let marcadores = [];
+
+// Executa a função 'obterDados().
+obterDados();
+
+// Define a função assíncrona 'obterDados()'.
+async function obterDados() {
+    // A função 'fetch' ler o arquivo 'lugares.json' e guarda na variável 'resposta'.
+    let resposta = await fetch('lugares.json');
+
+    // O operador 'await' pausa a função para receber o retorno da solicitação, que no caso é um objeto tipo JSON, 
+    lugares = await resposta.json();
+
+    // Observação: cada objeto da lista é acessado pelo seu índice, que começa com 0, 
+    // como 'lugares' tem nove objetos, os índices são de 0 a 8.
+    // O índice fica entre colchetes '[índice]' após o nome da lista de objetos, seguido de '.' e o nome da chave.
+    // Por exemplo, para acessar o valor da chave 'nome' do primeiro objeto da lista é usado 'lugares[0].nome' 
+
+    // Executa a função 'carregarMapa()'.
+    carregarMapa();
+}
+
+// Define a função 'carregarMapa()'.
+function carregarMapa() {
+
+    // Cria um ícone para os marcadores no mapa.
+    let icone = L.icon({
+        iconUrl: 'pino30_30.png', // Arquivo de imagem do ícone.
+        shadowUrl: 'pino30_30_sombra.png',
+        iconSize: [30, 30], // Tamanho do ícone em pixels.
+        iconAnchor: [15, 30], // Ponto no ícone que corresponde a localizaão do marcador.
+        shadowSize: [30, 30], // Tamanho do ícone da sombra.
+        shadowAnchor: [15, 30],  // Ponto do ícone da sombra que corresponde a localizaão do marcador.
+        popupAnchor: [0, -32] // Ponto quue o popup deve abrir em relação à posição do ícone.
+    });
+
+    // Cria o objeto 'meuMapa' com a vista centralizada na latitude e longitude de 'lugares[0]' com um zoom inicial de 10.
+    let meuMapa = L.map('mapaLeaflet').setView([lugares[0].lat, lugares[0].long], 10);
+
+    // Cria o mapa usando as imagens ('tiles') do OpenStreetMap (OSM), com a opção de atribuição citando a fonte da base de imagens do OSM.
+    let mapaBase = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    // Em seguida, o mapa é adicionado ao objeto 'meuMapa' com a função addTo().
+    mapaBase.addTo(meuMapa);
+
+    // A função 'for' é um loop (ou laço) com um número determinado de repetições,
+    // e geralmente tem a seguinte estrtura:
+    // (inicialização; condição; atualização){declarações executadas se a condição for verdadeira}
+    // Observação: '++' significa adicionar '1', portanto a expressão 'i++' é equivalente a 'i = i + 1'.
+    // Como 'lugares.length' retorna nove, neste caso as declaçõres de 'for' (o que está entre chaves {}) serão executadas nove vezes.
+    for (let i = 0; i < lugares.length; i++) {
+        // Cria e adiciona  marcador no mapa nas coordenadas especificadas em 'lugares[i]' com os respectivos ícones.
+        marcadores[i] = L.marker([lugares[i].lat, lugares[i].long], {
+            icon: icone
+        }).addTo(meuMapa);
+
+
+        // Cria a variável 'geoparqueInfo' com as informações dos geossítios contidas em 'lugares'.
+        // O elemento <a> HTML é usado para criar links, entretanto, para linkar a URL ('site') dos geossítios é usado:
+        // em vez de aspas '"', o sinal de crase '`'
+        // e o objeto é referenciado com ${}
+        // por exemplo:
+        // `<a href='${objetoURL}'>`
+        let geoparqueInfo = "<h2>" + lugares[i].nome + "</h2> Coordenadas: " + lugares[i].lat + ", " + lugares[i].long + "<br> Cidade: " + lugares[i].cidade + `<br> Site: <a href='${lugares[i].site}' target='blank'>` + lugares[i].site + "</a>";
+
+        // Inclui o texto de 'geoparqueInfo' nos marcadores[i], que são exibidos quando clicados.
+        marcadores[i].bindPopup(geoparqueInfo);
+    }
+}
